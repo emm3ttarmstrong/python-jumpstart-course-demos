8bd141aaca4705b27c6165b6c927c21a21c7d538
emm3ttarmstrong <lordrussano2@gmail.com>
2017-11-02 14:04:08 -0700

    finished app 2

---
+++ apps/02-guess-number-app/you_try/program.py
@@ -0,0 +1,24 @@
+import random
+
+print('-------------------------------------------')
+print('          GUESS THAT NUMBER GAME')
+print('-------------------------------------------')
+print()
+
+
+the_number = random.randint(0, 100)
+
+guess = -1
+
+while guess != the_number:
+
+    guess_text = input('Guess a number between 0 - 100: ')
+    guess = int(guess_text)
+
+    if guess < the_number:
+        print('Too low.')
+    elif guess > the_number:
+        print('Too high.')
+    else:
+        print('You win!')
+
