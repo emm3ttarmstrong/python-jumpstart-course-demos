f4147f0e4c295f694b390f8550dbcce2d3d16579
emm3ttarmstrong <lordrussano2@gmail.com>
2017-11-08 18:44:50 -0800

    Finished App 4: Journal

---
+++ apps/04_journal/you_try/journal.py
@@ -0,0 +1,41 @@
+import os
+
+def load(name):
+    '''
+    This method creates and loads a new journal.
+    :param name: name of journal that is being loaded.
+    :return: return entries in journal file, stripped of new lines.
+    '''
+
+    data = []
+    filename = get_full_pathname(name)
+
+    if os.path.exists(filename):
+        with open(filename) as fin:
+
+            for entry in fin.readlines():
+                data.append(entry.rstrip())
+
+    return data
+
+
+
+def save(name, joural_data):
+
+    filename = get_full_pathname(name)
+    print(".... Saving to: {}".format(filename))
+
+
+    with open(filename, 'w') as fout:
+
+        for entry in joural_data:
+            fout.write(entry + "\n")
+
+
+def get_full_pathname(name):
+    filename = os.path.abspath(os.path.join('.', 'journals' + name + '.jrl'))
+    return filename
+
+
+def add_entry(text, journal_data):
+    journal_data.append(text)
--- apps/04_journal/you_try/program.py
+++ apps/04_journal/you_try/program.py
@@ -1,3 +1,4 @@
+import journal
 
 def main():
     print_header()
@@ -15,20 +16,21 @@ def print_header():
 def run_event_loop():
 
     print('What do you want to do with your journal?')
-    cmd = None
-    journal_data = []
-
-    while cmd != 'x':
+    cmd = 'empty'
+    journal_name = 'default'
+    journal_data = journal.load(journal_name)
+    while cmd != 'x' and cmd:
         cmd = input('[l]ist entries, [a]dd an entry, e[x]it: ')
         cmd = cmd.lower().strip()
         if cmd == 'l':
             list_entries(journal_data)
         elif cmd == 'a':
             add_entries(journal_data)
-        elif cmd != 'x':
+        elif cmd != 'x' and cmd:
             print("Sorry, I'm not sure what '{}' means.".format(cmd))
 
     print("Goodbye!")
+    journal.save(journal_name, journal_data)
 
 def list_entries(data):
     print('Your journal entries: ')
@@ -38,8 +40,11 @@ def list_entries(data):
 
 def add_entries(data):
     text = input('Add your entry: ')
-    data = data.append(text)
+    journal.add_entry(text, data)
 
-main()
+if __name__ == __main__:
+    main()
+
+    
 
 
